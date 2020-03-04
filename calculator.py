d45ccec705b06ce0e44e194d43d9c6270785532d
hush <cd542738480@users.noreply.github.com>
2020-03-04 16:28:06 +0800

    initial update

---
+++ calculator.py
@@ -0,0 +1,7 @@
+#script name     : calculator.py
+#author          :Chen Ding
+#created         :2020/03/04
+#lasted modified :2020/03/04
+#version         :1.0
+#discription     :do some simple calculation
+
